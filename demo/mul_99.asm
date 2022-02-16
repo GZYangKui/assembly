339bc97e420fe63af068d518367236616649545a
yangkui <752544765@qq.com>
2022-02-16 22:50:26 +0800

    fix bug

--- demo/mul_99.asm
+++ demo/mul_99.asm
@@ -6,33 +6,82 @@ section .data
  SYS_write   equ          1
  STD_OUT     equ          1
  EXIT_SUCC   equ          0
- rows        equ          9
- columns     equ          9
  LF          equ          10
  NULL        equ          0
- outStr      dq         "AAAAAAAA",LF,NULL
- outStr1     dq         "qqqqqqqqqqqqqqqqqqqqqqqqqq"
+ row         dq           0
+ column      dq           0
+ preOutStr   dt           "*********格式化输出9*9乘法表**********",LF,NULL
+ temp        db           NULL
+
+
+section .bss
+;保存20个字节用于后续字符串输出
+outStr   resb       20
 
 section .text
 global _start
 _start:
-
-call printStr
-
+ push preOutStr
+ call printStr
+ rowLoop:
+ inc qword [row]
+ cmp qword [row],10
+ jge last
+ mov rdx,0
+ mov qword [column],0
+ colLoop:
+ inc qword [column]
+ cmp qword[column],10
+ jge rowLoop
+ mul dword [column]
+ push qword [row]
+ push qword [column]
+ push rax
+ call appendStr
+ mov rax,qword [row]
+ jmp colLoop
 
 last:
  mov rax,SYS_exit
  mov rdi,EXIT_SUCC
  syscall
 
-global printStr
-printStr:
+global appendStr
+appendStr:
  push rbp
  mov rbp,rsp
  mov rbx,outStr
+ ;结算结果
+ mov bl,byte [rsp+24]
+ add bl,48
+ mov byte[temp],bl
+ mov byte[outStr],bl
+ mov bl,'*'
+ mov byte[outStr+1],bl
+ add bl,48
+ mov bl,byte [rsp+16]
+ mov byte[outStr+2],bl
+ mov bl,'='
+ mov byte[outStr+3],bl
+ mov bl,byte [rsp+8]
+ mov byte[outStr+4],bl
+ mov bl,LF
+ mov byte[outStr+5],bl
+ push outStr
+ call printStr
+ mov rsp,rbp
+ pop rbp
+ ret
+global printStr
+
+;将字符串地址通过参数形式传入当前函数
+printStr:
+ push rbp
+ mov rbp,rsp
+ mov rbx,qword [rsp+16]
  mov rdx,0
  strCount:
- cmp byte [rbx],LF
+ cmp byte [rbx],NULL
  je strCountEnd
  inc rdx
  inc rbx
@@ -40,8 +89,20 @@ printStr:
  strCountEnd:
  mov rax,SYS_write
  mov rdi,STD_OUT
- mov rsi,outStr
+ mov rsi,qword [rsp+16]
  syscall
+ mov rsp,rbp
+ pop rbp
+ ret
+
+;将阿拉伯数字转换为一个完整的字符串
+global numToAscii
+numToAscii:
+ push rbp
+ mov rbp,rsp
+ push r8
+ mov r8,qword[rsp+16]
+ pop r8
  mov rsp,rbp
  pop rbp
  ret
